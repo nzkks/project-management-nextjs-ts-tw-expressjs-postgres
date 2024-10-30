9b53449fb0a6f513586d020a4ea62cc19550a962
nzkks <kkshanthosh@gmail.com>
2024-10-30 22:01:25 +1300

    Client: API: add getTasks api endpoint. And show the tasks datagrid in the Homepage

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,10 +1,55 @@
+"use client";
+
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+import { dataGridClassNames, dataGridSxStyles } from "@/lib/utils";
+import { useGetTasksQuery } from "@/state/api";
+import { useAppSelector } from "../redux";
+
+const taskColumns: GridColDef[] = [
+  { field: "title", headerName: "Title", width: 200 },
+  { field: "status", headerName: "Status", width: 150 },
+  { field: "priority", headerName: "Priority", width: 150 },
+  { field: "dueDate", headerName: "Due Date", width: 150 },
+];
+
 const HomePage = () => {
+  const {
+    data: tasks,
+    isLoading: tasksLoading,
+    isError: tasksError,
+  } = useGetTasksQuery({ projectId: parseInt("1") });
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  if (tasksLoading) return <div>Loading..</div>;
+  if (tasksError || !tasks) return <div>Error fetching data</div>;
+
   return (
     <div className="container h-full w-[100%] bg-gray-100 bg-transparent p-8">
       <div className="mb-5 w-full">
         <h1 className="text-2xl font-semibold dark:text-white">
           Project Management Dashboard
         </h1>
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <div className="rounded-lg bg-white p-4 shadow dark:bg-dark-secondary md:col-span-2">
+          <h3 className="mb-4 text-lg font-semibold dark:text-white">
+            Your Tasks
+          </h3>
+          <div style={{ height: 400, width: "100%" }}>
+            <DataGrid
+              rows={tasks}
+              columns={taskColumns}
+              checkboxSelection
+              loading={tasksLoading}
+              getRowClassName={() => "data-grid-row"}
+              getCellClassName={() => "data-grid-cell"}
+              className={dataGridClassNames}
+              sx={dataGridSxStyles(isDarkMode)}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -8,18 +8,77 @@ export interface Project {
   endDate?: string;
 }
 
+export enum Priority {
+  Urgent = "Urgent",
+  High = "High",
+  Medium = "Medium",
+  Low = "Low",
+  Backlog = "Backlog",
+}
+
+export enum Status {
+  ToDo = "To Do",
+  WorkInProgress = "Work In Progress",
+  UnderReview = "Under Review",
+  Completed = "Completed",
+}
+
+export interface User {
+  userId?: number;
+  username: string;
+  email: string;
+  profilePictureUrl?: string;
+  cognitoId?: string;
+  teamId?: number;
+}
+
+export interface Attachment {
+  id: number;
+  fileURL: string;
+  fileName: string;
+  taskId: number;
+  uploadedById: number;
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  status?: Status;
+  priority?: Priority;
+  tags?: string;
+  startDate?: string;
+  dueDate?: string;
+  points?: number;
+  projectId: number;
+  authorUserId?: number;
+  assignedUserId?: number;
+
+  author?: User;
+  assignee?: User;
+  comments?: Comment[];
+  attachments?: Attachment[];
+}
+
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
   }),
   reducerPath: "api",
-  tagTypes: ["Projects"],
+  tagTypes: ["Projects", "Tasks"],
   endpoints: (build) => ({
     getProjects: build.query<Project[], void>({
       query: () => "projects", // "baseUrl/projects". Example: "http://localhost:8000/projects"
       providesTags: ["Projects"],
     }),
+    getTasks: build.query<Task[], { projectId: number }>({
+      query: ({ projectId }) => `tasks?projectId=${projectId}`,
+      providesTags: (result) =>
+        result
+          ? result.map(({ id }) => ({ type: "Tasks" as const, id }))
+          : [{ type: "Tasks" as const }],
+    }),
   }),
 });
 
-export const { useGetProjectsQuery } = api;
+export const { useGetProjectsQuery, useGetTasksQuery } = api;
