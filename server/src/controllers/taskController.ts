d12f79a820c5b6422573a3ae69431c7a1111fc47
nzkks <kkshanthosh@gmail.com>
2024-10-30 19:31:04 +1300

    Server: add create Task related route and controller

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -23,3 +23,39 @@ export const getTasks = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({ message: `Error retrieving tasks: ${error.message}` });
   }
 };
+
+export const createTask = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const {
+      title,
+      description,
+      status,
+      priority,
+      tags,
+      startDate,
+      dueDate,
+      points,
+      projectId,
+      authorUserId,
+      assignedUserId,
+    } = req.body;
+    const newTask = await prisma.task.create({
+      data: {
+        title,
+        description,
+        status,
+        priority,
+        tags,
+        startDate,
+        dueDate,
+        points,
+        projectId,
+        authorUserId,
+        assignedUserId,
+      },
+    });
+    res.status(201).json(newTask);
+  } catch (error: any) {
+    res.status(500).json({ message: `Error creating task: ${error.message}` });
+  }
+};
--- server/src/routes/taskRoutes.ts
+++ server/src/routes/taskRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
-import { getTasks } from '../controllers/taskController';
+import { getTasks, createTask } from '../controllers/taskController';
 
 const router = Router();
 
 router.get('/', getTasks);
+router.post('/', createTask);
 
 export default router;
