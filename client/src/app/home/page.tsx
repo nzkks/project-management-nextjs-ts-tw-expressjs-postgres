cc3e82aefa4d5dda04815518913fad9c87f24a23
nzkks <kkshanthosh@gmail.com>
2024-10-30 21:50:15 +1300

    Client: create Home page

---
+++ client/src/app/home/page.tsx
@@ -0,0 +1,13 @@
+const HomePage = () => {
+  return (
+    <div className="container h-full w-[100%] bg-gray-100 bg-transparent p-8">
+      <div className="mb-5 w-full">
+        <h1 className="text-2xl font-semibold dark:text-white">
+          Project Management Dashboard
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,5 @@
+import HomePage from "./home/page";
+
 export default function Home() {
-  return <>Hi</>;
+  return <HomePage />;
 }
