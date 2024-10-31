d458faeedd10e92129dab7dd991d69e17347d7b5
nzkks <kkshanthosh@gmail.com>
2024-11-01 11:12:44 +1300

    Client: Board view: show tasks count next to each status column headers

--- client/src/components/project/BoardView/index.tsx
+++ client/src/components/project/BoardView/index.tsx
@@ -19,7 +19,7 @@ const BoardView = ({ tasks }: Props) => {
   return (
     <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 xl:grid-cols-4">
       {taskStatus.map((status) => (
-        <TaskColumn key={status} status={status} />
+        <TaskColumn key={status} status={status} tasks={tasks} />
       ))}
     </div>
   );
@@ -27,9 +27,12 @@ const BoardView = ({ tasks }: Props) => {
 
 type TaskColumnProps = {
   status: string;
+  tasks: Task[];
 };
 
-const TaskColumn = ({ status }: TaskColumnProps) => {
+const TaskColumn = ({ status, tasks }: TaskColumnProps) => {
+  const tasksCount = tasks?.filter((task) => task.status === status).length;
+
   return (
     <div className={`sl:py-4 rounded-lg py-2 xl:px-2`}>
       <div className="mb-3 flex w-full">
@@ -39,7 +42,13 @@ const TaskColumn = ({ status }: TaskColumnProps) => {
         />
         <div className="flex w-full items-center justify-between rounded-e-lg bg-white px-5 py-4 dark:bg-dark-secondary">
           <h3 className="flex items-center text-lg font-semibold dark:text-white">
-            {status}
+            {status}{" "}
+            <span
+              className="ml-2 inline-block rounded-full bg-gray-200 p-1 text-center text-sm leading-none dark:bg-dark-tertiary"
+              style={{ width: "1.5rem", height: "1.5rem" }}
+            >
+              {tasksCount}
+            </span>
           </h3>
         </div>
       </div>
