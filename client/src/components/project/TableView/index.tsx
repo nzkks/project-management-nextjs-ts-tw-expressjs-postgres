a052f9fefd3398af68642f8eaa6f3d8aee89c035
nzkks <kkshanthosh@gmail.com>
2024-10-31 17:43:21 +1300

    Client: Table View: Show the task details using a data table

--- client/src/components/project/TableView/index.tsx
+++ client/src/components/project/TableView/index.tsx
@@ -1,19 +1,82 @@
 import React from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 import { Task } from "@/state/api";
+import { useAppSelector } from "@/app/redux";
+import { dataGridClassNames, dataGridSxStyles } from "@/lib/utils";
+
+const columns: GridColDef[] = [
+  {
+    field: "title",
+    headerName: "Title",
+    width: 100,
+  },
+  {
+    field: "description",
+    headerName: "Description",
+    width: 200,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 130,
+    renderCell: (params) => (
+      <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+        {params.value}
+      </span>
+    ),
+  },
+  {
+    field: "priority",
+    headerName: "Priority",
+    width: 75,
+  },
+  {
+    field: "tags",
+    headerName: "Tags",
+    width: 130,
+  },
+  {
+    field: "startDate",
+    headerName: "Start Date",
+    width: 130,
+  },
+  {
+    field: "dueDate",
+    headerName: "Due Date",
+    width: 130,
+  },
+  {
+    field: "author",
+    headerName: "Author",
+    width: 150,
+    renderCell: (params) => params.value?.author || "Unknown",
+  },
+  {
+    field: "assignee",
+    headerName: "Assignee",
+    width: 150,
+    renderCell: (params) => params.value?.assignee || "Unassigned",
+  },
+];
 
 type Props = {
   tasks: Task[];
 };
 
 const TableView = ({ tasks }: Props) => {
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
   return (
     <div className="h-[540px] w-full px-4 pb-8 xl:px-6">
       <div className="pt-5">Table View</div>
 
-      {tasks.map((task) => (
-        <div key={task.id}>{task.title}</div>
-      ))}
+      <DataGrid
+        rows={tasks || []}
+        columns={columns}
+        className={dataGridClassNames}
+        sx={dataGridSxStyles(isDarkMode)}
+      />
     </div>
   );
 };
