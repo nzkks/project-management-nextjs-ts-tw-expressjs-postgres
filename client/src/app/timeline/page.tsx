27b56a55e89dd79fe0bf8ac692dbbd4e1167da0c
nzkks <kkshanthosh@gmail.com>
2024-11-01 18:06:33 +1300

    Client: Timeline: Show the timeline for all projects

--- client/src/app/timeline/page.tsx
+++ client/src/app/timeline/page.tsx
@@ -1,8 +1,43 @@
-import React from "react";
+"use client";
 
+import React, { useMemo, useState } from "react";
+import { DisplayOption, Gantt, ViewMode } from "gantt-task-react";
+import "gantt-task-react/dist/index.css";
+
+import { useGetProjectsQuery } from "@/state/api";
+import { useAppSelector } from "../redux";
 import Header from "@/components/Header";
 
+type TaskTypeItems = "task" | "milestone" | "project";
+
 const Timeline = () => {
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  const { data: projects, isLoading, isError } = useGetProjectsQuery();
+
+  const [displayOptions, setDisplayOptions] = useState<DisplayOption>({
+    viewMode: ViewMode.Month,
+    locale: "en-US",
+  });
+
+  const ganttTasks = useMemo(() => {
+    return (
+      projects?.map((project) => ({
+        start: new Date(project.startDate as string),
+        end: new Date(project.endDate as string),
+        name: project.name,
+        id: `Project-${project.id}`,
+        type: "project" as TaskTypeItems,
+        progress: 50,
+        isDisabled: false,
+      })) || []
+    );
+  }, [projects]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError || !projects)
+    return <div>An error occurred while fetching projects</div>;
+
   return (
     <div className="max-w-full p-8">
       <header className="mb-4 flex items-center justify-between">
@@ -10,7 +45,17 @@ const Timeline = () => {
       </header>
 
       <div className="overflow-hidden rounded-md bg-white shadow dark:bg-dark-secondary dark:text-white">
-        <div className="timeline">Gantt Timeline</div>
+        <div className="timeline">
+          <Gantt
+            tasks={ganttTasks}
+            {...displayOptions}
+            columnWidth={displayOptions.viewMode === ViewMode.Month ? 150 : 100}
+            listCellWidth="100px"
+            projectBackgroundColor={isDarkMode ? "#101214" : "#1f2937"}
+            projectProgressColor={isDarkMode ? "#1f2937" : "#aeb8c2"}
+            projectProgressSelectedColor={isDarkMode ? "#000" : "#9ba1a6"}
+          />
+        </div>
       </div>
     </div>
   );
