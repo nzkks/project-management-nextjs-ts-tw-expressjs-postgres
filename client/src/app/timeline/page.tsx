c5021ba90648b29bd90855de5d4265d351585cdf
nzkks <kkshanthosh@gmail.com>
2024-11-01 18:08:36 +1300

    Client: Timeline: Show view mode change options

--- client/src/app/timeline/page.tsx
+++ client/src/app/timeline/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useMemo, useState } from "react";
+import React, { ChangeEvent, useMemo, useState } from "react";
 import { DisplayOption, Gantt, ViewMode } from "gantt-task-react";
 import "gantt-task-react/dist/index.css";
 
@@ -34,6 +34,13 @@ const Timeline = () => {
     );
   }, [projects]);
 
+  const handleViewModeChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setDisplayOptions((prev) => ({
+      ...prev,
+      viewMode: event.target.value as ViewMode,
+    }));
+  };
+
   if (isLoading) return <div>Loading...</div>;
   if (isError || !projects)
     return <div>An error occurred while fetching projects</div>;
@@ -42,6 +49,17 @@ const Timeline = () => {
     <div className="max-w-full p-8">
       <header className="mb-4 flex items-center justify-between">
         <Header name="Projects Timeline" />
+        <div className="relative inline-block w-64">
+          <select
+            className="focus:shadow-outline block w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 pr-8 leading-tight shadow hover:border-gray-500 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"
+            value={displayOptions.viewMode}
+            onChange={handleViewModeChange}
+          >
+            <option value={ViewMode.Day}>Day</option>
+            <option value={ViewMode.Week}>Week</option>
+            <option value={ViewMode.Month}>Month</option>
+          </select>
+        </div>
       </header>
 
       <div className="overflow-hidden rounded-md bg-white shadow dark:bg-dark-secondary dark:text-white">
