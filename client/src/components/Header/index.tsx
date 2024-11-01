8af8fdacff6aac95e68388c3ff2b6414b6ef2db7
nzkks <kkshanthosh@gmail.com>
2024-11-01 22:32:17 +1300

    Client: Header: show smaller title text when needed

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 type Props = {
   name: string;
+  buttonComponent?: ReactNode;
+  isSmallText?: boolean;
 };
 
-const Header = ({ name }: Props) => {
+const Header = ({ name, buttonComponent, isSmallText }: Props) => {
   return (
-    <div className="mb-5 w-full">
-      <h1 className="text-2xl font-semibold dark:text-white">{name}</h1>
+    <div className="mb-5 flex w-full items-center justify-between">
+      <h1
+        className={`${isSmallText ? "text-lg" : "text-2xl"} font-semibold dark:text-white`}
+      >
+        {name}
+      </h1>
+
+      {buttonComponent}
     </div>
   );
 };
