07cb79ed6f536c112ff2f5d744452d91599675b0
nzkks <kkshanthosh@gmail.com>
2024-11-05 22:14:00 +1300

    Client: Navbar: remove Search input

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import {
   Menu as MenuIcon,
-  Search as SearchIcon,
   Settings as SettingsIcon,
   User as UserIcon,
   Sun as SunIcon,
@@ -21,7 +20,6 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
-      {/* Search Bar */}
       <div className="flex items-center gap-8">
         {!isSidebarCollapsed ? null : (
           <button
@@ -30,14 +28,6 @@ const Navbar = () => {
             <MenuIcon className="h-8 w-8 dark:text-white" />
           </button>
         )}
-        <div className="relative flex h-min w-[200px]">
-          <SearchIcon className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
-          <input
-            className="w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white"
-            type="search"
-            placeholder="Search..."
-          />
-        </div>
       </div>
 
       {/* Icons */}
