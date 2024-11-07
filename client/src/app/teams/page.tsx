ccb268d1e0ec86a45f3fe95c5b955cce613ed494
nzkks <kkshanthosh@gmail.com>
2024-11-08 11:30:51 +1300

    Teams page: Show members

--- client/src/app/teams/page.tsx
+++ client/src/app/teams/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
 import {
   DataGrid,
   GridColDef,
@@ -9,7 +10,7 @@ import {
   GridToolbarFilterButton,
 } from "@mui/x-data-grid";
 
-import { useGetTeamsQuery } from "@/state/api";
+import { useGetTeamsQuery, User } from "@/state/api";
 import { useAppSelector } from "../redux";
 import { dataGridClassNames, dataGridSxStyles } from "@/lib/utils";
 import Header from "@/components/Header";
@@ -24,11 +25,77 @@ const CustomToolbar = () => (
 const columns: GridColDef[] = [
   { field: "id", headerName: "Team ID", width: 100 },
   { field: "teamName", headerName: "Team Name", width: 200 },
-  { field: "productOwnerUsername", headerName: "Product Owner", width: 200 },
   {
-    field: "projectManagerUsername",
+    field: "productOwner",
+    headerName: "Product Owner",
+    width: 150,
+    renderCell: (params) => (
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="h-9 w-9">
+          <Image
+            src={`/${params.row.productOwner.profilePictureUrl}`}
+            alt={params.row.productOwner.username}
+            width={100}
+            height={50}
+            className="h-full rounded-full object-cover"
+          />
+        </div>
+        <div className="ml-2">{params.row.productOwner.username}</div>
+      </div>
+    ),
+  },
+  {
+    field: "projectManager",
     headerName: "Project Manager",
-    width: 200,
+    width: 150,
+    renderCell: (params) => (
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="h-9 w-9">
+          <Image
+            src={`/${params.row.projectManager.profilePictureUrl}`}
+            alt={params.row.projectManager.username}
+            width={100}
+            height={50}
+            className="h-full rounded-full object-cover"
+          />
+        </div>
+        <div className="ml-2">{params.row.projectManager.username}</div>
+      </div>
+    ),
+  },
+  {
+    field: "teamSize",
+    headerName: "Team Size",
+    width: 100,
+  },
+  {
+    field: "teamMembers",
+    headerName: "Team Members",
+    width: 150,
+    renderCell: (params) => {
+      return (
+        params.row.teamMembers.length > 0 && (
+          <div className="mt-3 flex items-center justify-between">
+            <div className="flex -space-x-[6px] overflow-hidden">
+              {params.row.teamMembers.map((member: User, idx: number) => (
+                <div key={idx} className="has-tooltip">
+                  <span className="tooltip -mt-8 rounded bg-gray-100 p-1 shadow-lg dark:bg-black">
+                    {member.username}
+                  </span>
+                  <Image
+                    src={`/${member.profilePictureUrl}`}
+                    alt={member.username}
+                    width={30}
+                    height={30}
+                    className="h-8 w-8 rounded-full border-2 border-white object-cover dark:border-dark-secondary"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        )
+      );
+    },
   },
 ];
 
--- server/src/controllers/teamController.ts
+++ server/src/controllers/teamController.ts
@@ -5,8 +5,41 @@ const prisma = new PrismaClient();
 
 export const getTeams = async (req: Request, res: Response) => {
   try {
-    const teams = await prisma.team.findMany();
-    res.status(201).json(teams);
+    const teams = await prisma.team.findMany({
+      include: {
+        user: true,
+      },
+    });
+
+    const teamsWithMembers = await Promise.all(
+      teams.map(async team => {
+        const productOwner = await prisma.user.findUnique({
+          where: { userId: team.productOwnerUserId! },
+          select: { username: true, profilePictureUrl: true },
+        });
+
+        const projectManager = await prisma.user.findUnique({
+          where: { userId: team.projectManagerUserId! },
+          select: { username: true, profilePictureUrl: true },
+        });
+
+        const teamMembers = await prisma.user.findMany({
+          where: { teamId: team.id },
+          select: { username: true, profilePictureUrl: true },
+        });
+
+        return {
+          id: team.id,
+          teamName: team.teamName,
+          productOwner,
+          projectManager,
+          teamSize: teamMembers.length,
+          teamMembers,
+        };
+      })
+    );
+
+    res.status(201).json(teamsWithMembers);
   } catch (error: any) {
     res.status(500).json({ message: `Error retrieving teams: ${error.message}` });
   }
