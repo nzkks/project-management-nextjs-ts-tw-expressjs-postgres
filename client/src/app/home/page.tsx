b5e7264462ccd71a775fe8a6686e9e8f9c5a015f
nzkks <kkshanthosh@gmail.com>
2024-11-08 21:01:37 +1300

    Client: Home page: show "Task Priority Distribution" as a bar chart

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,9 +1,19 @@
 "use client";
 
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 import { dataGridClassNames, dataGridSxStyles } from "@/lib/utils";
-import { useGetTasksQuery } from "@/state/api";
+import { Priority, Task, useGetTasksQuery } from "@/state/api";
 import { useAppSelector } from "../redux";
 import Header from "@/components/Header";
 
@@ -26,12 +36,63 @@ const HomePage = () => {
   if (tasksLoading) return <div>Loading..</div>;
   if (tasksError || !tasks) return <div>Error fetching data</div>;
 
+  const priorityCount = tasks.reduce(
+    (acc: Record<string, number>, task: Task) => {
+      const { priority } = task;
+      acc[priority as Priority] = (acc[priority as Priority] || 0) + 1;
+      return acc;
+    },
+    {},
+  );
+
+  const taskDistribution = Object.keys(priorityCount).map((key) => ({
+    name: key,
+    count: priorityCount[key],
+  }));
+
+  const chartColors = isDarkMode
+    ? {
+        bar: "#8884d8",
+        barGrid: "#303030",
+        pieFill: "#4A90E2",
+        text: "#FFFFFF",
+      }
+    : {
+        bar: "#8884d8",
+        barGrid: "#E0E0E0",
+        pieFill: "#82ca9d",
+        text: "#000000",
+      };
+
   return (
     <div className="container h-full w-[100%] bg-gray-100 bg-transparent p-8">
       <div className="mb-5 w-full">
         <Header name="Project Management Dashboard" />
       </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <div className="rounded-lg bg-white p-4 shadow dark:bg-dark-secondary">
+          <h3 className="mb-4 text-lg font-semibold dark:text-white">
+            Task Priority Distribution
+          </h3>
+          <ResponsiveContainer width="100%" height={300}>
+            <BarChart data={taskDistribution}>
+              <CartesianGrid
+                strokeDasharray="3 3"
+                stroke={chartColors.barGrid}
+              />
+              <XAxis dataKey="name" stroke={chartColors.text} />
+              <YAxis stroke={chartColors.text} />
+              <Tooltip
+                contentStyle={{
+                  width: "min-content",
+                  height: "min-content",
+                }}
+              />
+              <Legend />
+              <Bar dataKey="count" fill={chartColors.bar} />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
         <div className="rounded-lg bg-white p-4 shadow dark:bg-dark-secondary md:col-span-2">
           <h3 className="mb-4 text-lg font-semibold dark:text-white">
             Your Tasks
