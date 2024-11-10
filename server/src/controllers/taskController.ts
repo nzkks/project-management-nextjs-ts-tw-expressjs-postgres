264d89de942e002da915b208d2071a2b804a03e6
nzkks <kkshanthosh@gmail.com>
2024-11-11 10:34:40 +1300

    Server: add 'get tasks by user id; controller and a route

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -24,6 +24,25 @@ export const getTasks = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
+export const getTasksByUserId = async (req: Request, res: Response): Promise<void> => {
+  const { userId } = req.params;
+
+  try {
+    const tasks = await prisma.task.findMany({
+      where: {
+        OR: [{ authorUserId: Number(userId) }, { assignedUserId: Number(userId) }],
+      },
+      include: {
+        author: true,
+        assignee: true,
+      },
+    });
+    res.status(201).json(tasks);
+  } catch (error: any) {
+    res.status(500).json({ message: `Error retrieving user tasks: ${error.message}` });
+  }
+};
+
 export const createTask = async (req: Request, res: Response): Promise<void> => {
   try {
     const {
--- server/src/routes/taskRoutes.ts
+++ server/src/routes/taskRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
-import { getTasks, createTask, updateTaskStatus } from '../controllers/taskController';
+import { getTasks, createTask, updateTaskStatus, getTasksByUserId } from '../controllers/taskController';
 
 const router = Router();
 
 router.get('/', getTasks);
 router.post('/', createTask);
 router.patch('/:taskId/status', updateTaskStatus);
+router.get('/user/:userId', getTasksByUserId);
 
 export default router;
